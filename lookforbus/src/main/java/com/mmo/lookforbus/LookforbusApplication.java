3df44e5dd4ee50b4cb1f5dce939cca0bea6af6ff
Tungbk09 <97616371+Tungbk09@users.noreply.github.com>
2022-04-23 19:43:27 +0700

    init code

---
+++ lookforbus/src/main/java/com/mmo/lookforbus/LookforbusApplication.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookforbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookforbusApplication.class, args);
+	}
+
+}
---
+++ lookforbus/src/test/java/com/mmo/lookforbus/LookforbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmo.lookforbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookforbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
